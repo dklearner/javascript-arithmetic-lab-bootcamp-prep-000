d16315c44dee5cadbd9347ef2f6e81d428d4ea8f
Danielle Kelly <kelly.danielle2@gmail.com>
2018-07-15 17:35:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ var b= 5
 function add(a,b) {
   return(a+b)
 }
+
+function subtract(a,b){
+  return(a-b)
+}
+
+function multiply(a,b){
+  return(a*b)
+}
+
+function divide(a,b){
+  return(a/b)
+}
+
